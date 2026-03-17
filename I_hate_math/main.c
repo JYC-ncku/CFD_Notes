9c8f816727c76a0f2d970199522fb11e2775ac27
JunYi <nicejunyi@gmail.com>
2026-03-17 17:32:55 +0800

    Delete H, we don't need it.

--- I_hate_math/main.c
+++ I_hate_math/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-//#include "compute_T.h"
+#include "compute_T.h"
 
 
 int main(){
 	float T_new, Cv, H, e, R; // R is mean Residual function
 	float a1, a2, a3, a4, a5, a6;//coefficients of compute Cv and H
-	float T = 300;
+	float T = 300.0;
 	float Tolerance = 0.001;
         float R_gas = 296.8; // R of N2 is 296.8 J/(kg*K)
-        float e_target = 2000000;
         float error = 100.0;
-
+        float e_target = (770*400);
+        
 	while(error > Tolerance){
 		//Decide whether to use the high-temperature or low-temperature coefficients.
 		if(T <= 1000){
@@ -32,21 +32,21 @@ int main(){
 		}
 		//Compute T
 		Cv = R_gas * (a1 + a2*T + (a3 * pow(T,2)) + (a4 * pow(T,3)) + (a5 * pow(T,4))) - R_gas; //Cv = (Cp/R)dimensionless * Rgas - Rgas
-		H = R_gas * T * (a1 + ((a2/2.0)*T) + ((a3/3.0) * pow(T,2)) + ((a4/4.0) * pow(T,3)) + ((a5/5.0) * pow(T,4)) + (a6/T)); //H = (H/RT)dimensionless * RgasT
-		e = H - R_gas * T; //H = E + PV => h = e + Pv; Pv = RT => h = e + RT => e = h - RT.
+		//H = R_gas * T * (a1 + ((a2/2.0)*T) + ((a3/3.0) * pow(T,2)) + ((a4/4.0) * pow(T,3)) + ((a5/5.0) * pow(T,4)) + (a6/T)); //H = (H/RT)dimensionless * RgasT
+		e = Cv * T;
 		R = e - e_target;
 		T_new = T - (R/Cv);
 		//Preventing the temperature from rising too quickly
-		if(T_new < 300){
+/*		if(T_new < 300){
 		T_new = 300;
 		}else if(T_new > 5000){
 		T_new = 5000;
 		}
-
+*/
 		error = fabs(T_new - T);
 		T = T_new;
 	}
 printf("Final temperature is %.9f K.\n", T);
-printf("\nHello world!\n");
+
 return 0;
 }
